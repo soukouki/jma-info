9d3c303b813dcba4dd9fffd6e0f8414219b8d8ea
soukouki <soukouki0@yahoo.co.jp>
2016-12-30 13:54:54 +0900

    ファイル分けとか

--- jma-info.rb
+++ jma-info.rb
@@ -1,46 +1,20 @@
 # encoding: UTF-8
 
-require "open-uri"
-
-require "nokogiri"
-
-class UriAndTitle
-	attr_accessor :uri, :title
-	def initialize
-		yield(self)
-	end
-	
-	def to_s
-		"uri=>#{uri}, title=>#{title}"
-	end
-	def eql? pair
-		uri == pair.uri && title == pair.title
-	end
-	def hash
-		uri.hash + title.hash
-	end
-end
-
-def get_uri_list
-	doc = Nokogiri::HTML(open("http://api.aitc.jp/jmardb/"), nil, 'UTF-8')
-	trs = doc.xpath("/html/body/div[3]/table/tr[position()!=1]")
-	trs.map do |tr|
-		UriAndTitle.new do |a|
-			a.uri = File.join("http://api.aitc.jp/jmardb/", tr.xpath("td[6]/a/@href").text.split(";")[0])
-			a.title = tr.xpath("td/a").text
-		end
-	end
-end
+require "./jma-info/get-uri-list"
 
 def sleep_up_to_even_number_minutes
 	sleep(((Time.now.min+1)%2*60)+(60-Time.now.sec))
 end
 
-old_uris = get_uri_list
+def get_info(uri_and_title)
+	uri_and_title.title
+end
+
+old_uris = []#get_uri_list
 loop do
 	puts Time.now
 	new_uris = get_uri_list
-	puts new_uris - old_uris
+	puts (new_uris-old_uris).map{|nu|get_info(nu)}
 	old_uris = new_uris
 	sleep_up_to_even_number_minutes
 end
---
+++ jma-info/get-uri-list.rb
@@ -0,0 +1,34 @@
+
+require "open-uri"
+
+require "nokogiri"
+
+class UriAndTitle
+	attr_accessor :uri, :title
+	def initialize
+		yield(self)
+	end
+	
+	def to_s
+		"uri=>#{uri}, title=>#{title}"
+	end
+	def eql? pair
+		uri == pair.uri && title == pair.title
+	end
+	def hash
+		uri.hash + title.hash
+	end
+end
+
+# 古いものを上にして表示する
+def get_uri_list
+	Nokogiri::HTML(open("http://api.aitc.jp/jmardb/"), nil, 'UTF-8')
+		.xpath("/html/body/div[3]/table/tr[position()!=1]")
+		.map do |tr|
+			UriAndTitle.new do |a|
+				a.uri = File.join("http://api.aitc.jp/jmardb/", tr.xpath("td[6]/a/@href").text.split(";")[0])
+				a.title = tr.xpath("td[6]/a").text
+			end
+		end
+		.reverse
+end
