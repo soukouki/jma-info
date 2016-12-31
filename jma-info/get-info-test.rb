824f1c825a0f5f4e8b1c8ac19061da3033c07c23
soukouki <soukouki0@yahoo.co.jp>
2016-12-31 14:00:00 +0900

    一般報

---
+++ jma-info/get-info-test.rb
@@ -0,0 +1,22 @@
+
+require "open-uri"
+require "./jma-info/get-info"
+
+=begin 一般報
+puts get_general_report("http://api.aitc.jp/jmardb/reports/2be7a5a8-11df-3fa4-954a-0b0b233884d2")
+puts get_general_report("http://api.aitc.jp/jmardb/reports/2f1a794e-12c5-3538-a683-72db002c2b9a")
+puts get_general_report("http://api.aitc.jp/jmardb/reports/7fa44ebf-64a1-32ab-9d99-379de6663211")
+=end
+
+=begin 概況
+puts get_general_weather_conditions("http://api.aitc.jp/jmardb/reports/6220814d-3290-3722-be82-52855f47f800")
+=end
+
+=begin 注意報
+puts get_alerm("http://api.aitc.jp/jmardb/reports/06ec89d7-67b5-3dad-9a54-5544ea0ae02d")
+puts get_alerm("http://api.aitc.jp/jmardb/reports/9f2c7b54-171d-3bab-9c0b-81bdc410db16")
+# 注意報解除
+puts get_alerm("http://api.aitc.jp/jmardb/reports/0854e7f4-44e6-32bf-a78a-500a628cbbdd")
+=end
+
+exit
--- jma-info/get-info.rb
+++ jma-info/get-info.rb
@@ -5,11 +5,23 @@ def get_doc uri
 	REXML::Document.new(open(uri))
 end
 
+def cleanly_text text
+	text
+		.gsub(/\u3000|\n(?!\n)/){""} # 全角スペースと単独の改行を消す
+		.gsub(/\n{2,}/){"\n"} # 連続の改行を一つの改行にする
+		.gsub(/^/){"\t"} # 行のはじめにタブを付ける
+end
+
 def get_general_report uri
+	doc=get_doc(uri)
+	doc.elements["Report/Head/Title"].text+"\n"+
+	cleanly_text(doc.elements["Report/Body/Comment/Text"].text.gsub("。"){"。\n\n"})
+end
+
+def get_general_weather_conditions uri
 	doc = get_doc(uri)
 	doc.elements["Report/Body/TargetArea/Name"].text+"\n"+
-	(doc.elements["Report/Body/Comment/Text"].text
-		.gsub(/\u3000|\n(?!\n)/){""}.gsub("\n\n"){"\n"}.gsub(/^/){"\t"})
+	cleanly_text(doc.elements["Report/Body/Comment/Text"].text)
 end
 
 def alerm_info doc
@@ -37,8 +49,13 @@ end
 
 def get_info(uri_and_title)
 	case uri_and_title.title
-	when "府県天気概況"
+	when # 一般報
+		/^全般台風情報/, "全般気象情報", "地方気象情報", "府県気象情報", "天気概況",
+		"全般週間天気予報", "地方週間天気予報", "スモッグ気象情報", "全般スモッグ気象情報",
+		"全般潮位情報", "地方潮位情報", "府県潮位情報", "府県海氷予報", "地方高温注意情報", "府県高温注意情報 "
 		uri_and_title.title+" : "+get_general_report(uri_and_title.uri)
+	when "府県天気概況"
+		uri_and_title.title+" : "+get_general_weather_conditions(uri_and_title.uri)
 	when "気象警報・注意報" # 無視
 	when "気象特別警報・警報・注意報"
 		uri_and_title.title+" : "+get_alerm(uri_and_title.uri)
