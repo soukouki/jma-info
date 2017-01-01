b4f8c8bd64a36c0891f5d2aaac9944982a42d850
soukouki <soukouki0@yahoo.co.jp>
2017-01-02 00:09:11 +0900

    ファイルに出力するように

--- jma-info.rb
+++ jma-info.rb
@@ -6,6 +6,19 @@ require "open-uri"
 require "./jma-info/get-uri-list"
 require "./jma-info/get-info"
 
+def file_write_and_print str
+	puts str
+	open("jma-info.log", "a"){|f|f.puts(str)}
+end
+
+def puts_info new_uris, old_uris
+	(new_uris-old_uris)
+		.lazy # 少しずつ表示していく
+		.map{|u|get_info(u)}
+		.select{|s|!s.nil?}
+		.each{|s|file_write_and_print s}
+end
+
 def sleep_up_to_even_number_minutes
 	sleep(((Time.now.min+1)%2*60)+(60-Time.now.sec))
 end
@@ -13,9 +26,9 @@ end
 old_uris = []#get_uri_list
 old_date = Time.now-120
 loop do
-	puts new_date = Time.now
+	file_write_and_print new_date = Time.now
 	new_uris = get_uri_list(old_date, new_date)
-	(new_uris-old_uris).lazy.map{|u|get_info(u)}.select{|s|!s.nil?}.each{|s|puts s} # 全体が揃う前に表示を始める
+	puts_info(new_uris, old_uris)
 	old_uris = new_uris
 	sleep_up_to_even_number_minutes
 end
