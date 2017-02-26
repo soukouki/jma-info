9d80902acb1c524ae1c1c0cd4d9192d69932c8cb
soukouki <soukouki0@yahoo.co.jp>
2017-02-26 23:15:58 +0900

    他フォルダからの実行時のバグを修正

--- jma-info.rb
+++ jma-info.rb
@@ -10,6 +10,14 @@ def multiple_puts lambdas, text
 	lambdas.each{|l|l.call(text)}
 end
 
+def absolute_path path
+	File::expand_path(path, __FILE__+"/..")
+end
+
+def file_open name, open_type="r", &open_block
+	File::open(absolute_path(name), open_type, &open_block)
+end
+
 def app arg
 	multiple_puts(arg[:puts], "起動しました。")
 	uris_cache = UrisCache.NewCache(60*10, Time.now) # 10分以上aticの更新時刻が気象庁の発表時刻が遅れないとする
@@ -34,13 +42,13 @@ def puts_info(puts_lambdas, updated_uris, now_time)
 end
 
 def file_appending f, s
-	File::open(File::expand_path("../"+f, __FILE__), "a"){|f|f.puts s}
+	file_open(f, "a"){|f|f.puts s}
 end
 
 def yomi browser, str
-	f_path = File.expand_path("../"+SecureRandom.uuid+".html", __FILE__)
-	File::open(f_path, "w"){|f|f.puts js_yomi(str)}
-	r = system(browser, f_path) or
+	file_path = absolute_path(SecureRandom.uuid+".html")
+	file_open(file_path, "w"){|f|f.puts js_yomi(str)}
+	r = system(browser, file_path) or
 		raise "ブラウザの立ち上げに失敗 : result=#{r.class}, browser=#{browser}, filepath=#{f_path}" # エラー処理
 	Thread.new{sleep 5; File::delete(f_path)}
 end
@@ -48,7 +56,7 @@ def js_yomi str
 	speak_text = '"'+str.lines.map{|s|s.chomp.gsub(/([^。])$/){$1+"。"}}.join(%!"+\n"!)+'"'
 	# 行末に「。」を追加しているのは、vivaldiで試したときに一息入れずに呼んだため。
 	print_text = str.gsub("\n"){"<br>\n"}.gsub("\t"){"　　"}
-	File::open("jma-info-data/yomi.html"){|io|io.set_encoding("utf-8").read}
+	file_open("jma-info-data/yomi.html"){|io|io.set_encoding("utf-8").read}
 		.gsub(/\#{([a-z_]+)}/){binding.local_variable_get($1)}
 end
 
