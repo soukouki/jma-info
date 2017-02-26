ef754f005551741f0951b28087c483d17a6e2b04
soukouki <soukouki0@yahoo.co.jp>
2017-02-26 23:15:58 +0900

    細かいバグを修正

--- jma-info.rb
+++ jma-info.rb
@@ -1,4 +1,4 @@
-# encoding: UTF-8
+﻿# encoding: UTF-8
 
 require "optparse"
 require "securerandom"
@@ -8,6 +8,14 @@ require_relative "jma-info/get-info"
 
 def multiple_puts lambdas, text
 	lambdas.each{|l|l.call(text)}
+end
+
+def absolute_path path
+	File::expand_path(path, __FILE__+"/..")
+end
+
+def file_open name, open_type="r", &open_block
+	File::open(absolute_path(name), open_type, &open_block)
 end
 
 def app arg
@@ -34,21 +42,21 @@ def puts_info(puts_lambdas, updated_uris, now_time)
 end
 
 def file_appending f, s
-	File::open(File::expand_path("../"+f, __FILE__), "a"){|f|f.puts s}
+	file_open(f, "a"){|f|f.puts s}
 end
 
 def yomi browser, str
-	f_path = File.expand_path("../"+SecureRandom.uuid+".html", __FILE__)
-	File::open(f_path, "w"){|f|f.puts js_yomi(str)}
-	r = system(browser, f_path) or
+	file_path = absolute_path(SecureRandom.uuid+".html")
+	file_open(file_path, "w"){|f|f.puts js_yomi(str)}
+	r = system(browser, file_path) or
 		raise "ブラウザの立ち上げに失敗 : result=#{r.class}, browser=#{browser}, filepath=#{f_path}" # エラー処理
-	Thread.new{sleep 5; File::delete(f_path)}
+	Thread.new{sleep 5; File::delete(file_path)}
 end
 def js_yomi str
 	speak_text = '"'+str.lines.map{|s|s.chomp.gsub(/([^。])$/){$1+"。"}}.join(%!"+\n"!)+'"'
 	# 行末に「。」を追加しているのは、vivaldiで試したときに一息入れずに呼んだため。
 	print_text = str.gsub("\n"){"<br>\n"}.gsub("\t"){"　　"}
-	File::open("jma-info-data/yomi.html"){|io|io.set_encoding("utf-8").read}
+	file_open("jma-info-data/yomi.html"){|io|io.set_encoding("utf-8").read}
 		.gsub(/\#{([a-z_]+)}/){binding.local_variable_get($1)}
 end
 
